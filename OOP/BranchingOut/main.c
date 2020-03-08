aa508814e8f92727c7adb4a8f52acc7f045d303b
WildUrti <46956225+WildUrti@users.noreply.github.com>
2020-03-08 17:01:02 +0200

    8.03.2020:17:00

--- OOP/BranchingOut/main.c
+++ OOP/BranchingOut/main.c
@@ -6,15 +6,6 @@
 #include "tests.h"
 #include "UserInterface.h"
 
-#include <string.h>
-#include <stdlib.h>
-
-typedef struct {
-	char name[60];
-	struct Nod* next;
-}Nod;
-
-
 int main()
 {
 	
@@ -26,18 +17,7 @@ int main()
 	CommandBasedUI commandBasedUI = createUI(&productService);
 
 	startProgram(&commandBasedUI);
-	/*
-	char** m = '\0';
-	char a[4] = "abc", b[4] = "def";
-	m = (char**)malloc(2 * sizeof(char*));
-	int i;
-	for (i= 0; i < 2; i++)
-		m[i] = (char*)malloc(sizeof(char) * 4);
 	
-	strcpy(m[0], a); // m[0] = *(m+0)
-	strcpy(m[1], b);
-	f(m);
-	*/
 	_CrtDumpMemoryLeaks();
 	return 0;
 }
